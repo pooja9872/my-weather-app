0d602f1cdb8758e2bb6f0f47160c6cfd132de8ea
pooja9872 <pooja_fw10_055@masai.school>
2022-06-24 22:21:14 +0530

    solving error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeadername:'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue:'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:'1b6a50598bmshbc5c099e25073e0p1e3e4bjsnc0c9aea981d5',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
-  XRapidAPIHostHeadername:'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue:'community-open-weather-map.p.rapidapi.com',
-  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue:'1b6a50598bmshbc5c099e25073e0p1e3e4bjsnc0c9aea981d5',
+  production: true,
+  // weatherApiBaseUrl:'https://community-open-weather-map.p.rapidapi.com/weather',
+  // XRapidAPIHostHeadername:'X-RapidAPI-Host',
+  // XRapidAPIHostHeaderValue:'community-open-weather-map.p.rapidapi.com',
+  // XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  // XRapidAPIKeyHeaderValue:'1b6a50598bmshbc5c099e25073e0p1e3e4bjsnc0c9aea981d5',
 };
 
 /*
