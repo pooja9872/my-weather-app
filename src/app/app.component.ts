adf75544d830a723c7f151a6b3830a31385504bc
Kumari Pooja HYD DIWID21 <pooja.kumari@zf.com>
2022-06-24 20:47:33 +0530

    weather app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weather-application';
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService:WeatherService){
+
+  }
+
+  cityName:string='London';
+  weatherData?:WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+  }
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+  }
+
+  private getWeatherData(cityName:string){
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next:(response)=>{
+        this.weatherData=response;
+        console.log(response)
+      }
+    })
+  }
+
 }
