f49d722972419f5d061615b518ba3d4853c4323b
pooja9872 <pooja_fw10_055@masai.school>
2022-12-17 23:56:55 +0530

    production http error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.openweathermap.org/data/2.5',
+  apiUrl: 'https://api.openweathermap.org/data/2.5',
   apiKey: '6655b18fd461e93c42b584f985edc534',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://api.openweathermap.org/data/2.5',
+  apiUrl: 'https://api.openweathermap.org/data/2.5',
   apiKey: '6655b18fd461e93c42b584f985edc534',
 };
 
